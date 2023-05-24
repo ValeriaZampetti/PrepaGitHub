160c5d50952556a3553e742e88827f048d495cb1
Valeria <vzampetti@correo.unimet.edu.ve>
2023-05-23 21:40:14 -0400

    lista

--- PrepaGitHub/src/prepagithub/Lista.java
+++ PrepaGitHub/src/prepagithub/Lista.java
@@ -9,5 +9,52 @@ package prepagithub;
  * @author valeriazampetti
  */
 public class Lista {
+    private Nodo pFirst;
+    private Nodo pLast;
+    private int size;
+
+    /**
+     * @return the pFirst
+     */
+    public Nodo getpFirst() {
+        return pFirst;
+    }
+
+    /**
+     * @param pFirst the pFirst to set
+     */
+    public void setpFirst(Nodo pFirst) {
+        this.pFirst = pFirst;
+    }
+
+    /**
+     * @return the pLast
+     */
+    public Nodo getpLast() {
+        return pLast;
+    }
+
+    /**
+     * @param pLast the pLast to set
+     */
+    public void setpLast(Nodo pLast) {
+        this.pLast = pLast;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    
     
 }
