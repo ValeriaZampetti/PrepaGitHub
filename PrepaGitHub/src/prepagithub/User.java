8f255068049fcf8eb099bd9fefefe4a70a6946bd
AndresARL <andres.rojas5722@gmail.com>
2023-05-23 21:31:53 -0400

    Create Class User

---
+++ PrepaGitHub/src/prepagithub/User.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prepagithub;
+
+/**
+ *
+ * @author Andres Rojas
+ */
+public class User {
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+}
